4d8db164a3034cce13f0f509db791b5c27f09664
Tomiwa <ajaomoshood@gmail.com>
2024-12-10 10:54:54 +0100

    paths changed, all components ready

---
+++ clothing-store/src/components/Home.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+</script>
+
+<template>
+     <nav class="navbar navbar-expand-md navbar-light bg-black py-1">
+        <div class="container-xxl">
+            <a href="#intro" class="navbar-brand" >
+                <i class="bi bi-book-half"></i>
+                <span class="fw-bold text-light">
+                  OLLA CLOTHING STORE
+                </span>
+            </a>
+            <!--toggle for mobile nav-->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+             <!--Navbar links-->
+               <div class="collapse-navbar-collapse justify-content-end align-center" 
+                id="main-nav">
+                    <ul class="navbar-nav">
+                     <li class="nav-item ">
+                      <a class="nav-link" href="Kid.html"  target="_blank"><p class="text-light">Kids Collection</p></a>
+                     </li>
+                     <li class="nav-item">
+                      <a class="nav-link" href="mens.html"  target="_blank"><p class="text-light">Mens Collection</p> </a>
+                     </li>
+                     <li class="nav-item ">
+                         <a class="nav-link " href="Ladies.html"  target="_blank"><p class="text-light">Ladies Collection</p></a>
+                     </li>
+                     
+                     </ul>
+      
+                </div>
+         </div>        
+    </nav> 
+
+    <section id="Intro">
+        
+        <div class="container-lg">
+            <div class="row justify-content-center align-items-center  ">
+                <div  class=" col-md-5 text-align-center">
+                    
+                        <div class="display-2 text-secondary"></div>   
+                 </div>
+                           
+            </div>           
+        </div>      
+    </section>
+    <article>
+        <div class="text-center">
+            <button type="button" class="btn btn-danger btn-lg  text-white px-5  align-item-center ">Get Started</button>
+        </div>
+        
+    </article>
+</template>
--- clothing-store/src/router/index.js
+++ clothing-store/src/router/index.js
@@ -1,5 +1,9 @@
+import Home from '@/components/Home.vue'
+import Kids from '@/components/Kids.vue'
+import Ladies from '@/components/Ladies.vue'
+import Men from '@/components/Men.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,16 +11,25 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/men',
+      name: 'men',
+      componenent: Men,
     },
+
+    {
+      path: '/kids',
+      name: 'kids',
+      componenent: Kids,
+    }
+    {
+      path: '/ladies',
+      name: 'ladies',
+      componenent: Ladies,
+    }
+
   ],
 })
 
